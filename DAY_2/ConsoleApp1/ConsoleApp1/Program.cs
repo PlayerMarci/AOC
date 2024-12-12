35711aa3da209175a197fae0a2a97ec0244d2dfe
Digi <marton@szabomarci.hu>
2024-12-12 21:07:42 +0100

    no sol yet :(

--- DAY_2/ConsoleApp1/ConsoleApp1/Program.cs
+++ DAY_2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -67,56 +67,69 @@ namespace ConsoleApp1
         {
             int counter = 0;
             foreach (var row in array) {
-                if (IsRowGood(row))
+                if (IsRowGood(row, 0))
                 {
                     counter++;
                     continue;
-                }
-                else { 
-
                 }
             }
 
             return counter;
         }
 
-        public static bool IsRowGood(List<int> row)
+        public static bool IsRowGood(List<int> row, int depth)
         {
-            for (int i = 1; i < row.Count - 1; i++)
+            if (depth > 1)
             {
-                int absDiffLeft = Math.Abs(row[i] - row[i - 1]);
-                int DiffRight = row[i + 1] - row[i];
-                int DiffLeft = row[i] - row[i - 1];
-                int absDiffRight = Math.Abs(DiffRight);
+                return false;
+            }
 
+            string line = "";
+            foreach (var item in row)
+            {
+                line += $"[{Convert.ToString(item)}]";
+            }
 
-                if (absDiffLeft <= 3 && absDiffRight <= 3 && ((DiffLeft > 0 && DiffRight > 0) || (DiffRight < 0 && DiffLeft < 0)))
+            Console.WriteLine($"{line}, depth: {depth}");
+
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            foreach (var item in row)
+            {
+                if (!dict.ContainsKey(item))
                 {
-                    continue;
-                }
-                else
+                    dict.Add(item, 1);
+                } else
                 {
-                    return false; 
+                    dict[item]++;
                 }
             }
 
-            return true;
-        }
-
-        public static int UnSafeNum_2()
-        {
-            int counter = 0;
-
-            foreach (var row in AOC.rows)
+            foreach (var item in dict)
             {
-                int rowcounter = 0;
-                for (int i = 1; i < row.Count - 1; i++)
+                // that means 3 or more of the same numbers are in the list
+                // so you have to go at least two deap
+                // so it is straight up bad
+                if (item.Value > 2)
+                {
+                    return false;
+                }
+
+                if (item.Value == 2)
+                {
+                    continue;
+                }
+            }
+
+            for (int i = 0; i < row.Count - 1; i++)
+            {
+                //normally
+                if (i - 1 >= 0 && i + 1 <= row.Count)
                 {
                     int absDiffLeft = Math.Abs(row[i] - row[i - 1]);
                     int DiffRight = row[i + 1] - row[i];
                     int DiffLeft = row[i] - row[i - 1];
                     int absDiffRight = Math.Abs(DiffRight);
-
 
                     if (absDiffLeft <= 3 && absDiffRight <= 3 && ((DiffLeft > 0 && DiffRight > 0) || (DiffRight < 0 && DiffLeft < 0)))
                     {
@@ -124,24 +137,85 @@ namespace ConsoleApp1
                     }
                     else
                     {
-                        rowcounter++;
+                        List<int> newrow = row;
+                        newrow.RemoveAt(i);
+                        return IsRowGood(newrow, depth + 1);
+                    }
+                } 
+                else
+                {   
+                    //if it is the left most piece
+                    if (i == 0)
+                    {
+                        int firstDiff = row[i + 1] - row[i];
+                        int secondDiff = row[i + 2] - row[i + 1];
+                        bool notZero = BothNegative(firstDiff, secondDiff) || BothPositive(firstDiff, secondDiff);
+                        if (Math.Abs(firstDiff) <= 3 && Math.Abs(secondDiff) <= 3  && notZero)
+                        {
+                            continue;
+                        } 
+                        else
+                        {
+                            List<int> newrow = row;
+                            newrow.RemoveAt(i);
+                            return IsRowGood(newrow, depth + 1);
+                        }
+                    } 
+                    else if (i == row.Count - 1) //if it is the right most
+                    {
+                        int firstDiff = row[i] - row[i - 1];
+                        int secondDiff = row[i - 1] - row[i - 2];
+                        bool notZero = BothNegative(firstDiff, secondDiff) || BothPositive(firstDiff, secondDiff);
+                        if (Math.Abs(firstDiff) <= 3 && Math.Abs(secondDiff) <= 3 && notZero)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            List<int> newrow = row;
+                            newrow.RemoveAt(i);
+                            return IsRowGood(newrow, depth + 1);
+                        }
                     }
                 }
+                
             }
-            return counter;
+
+            return true;
         }
+
+        public static bool BothPositive(int first, int second)
+        {
+            if (first > 0 && second > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool BothNegative(int first, int second)
+        {
+            if (first < 0 && second < 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+
         static void Main(string[] args)
         {
 
-            //string path = $@"C:\Users\szabomarton\Desktop\AOC\DAY_2\testinput";
-            string path = $@"C:\Users\szabomarton\Desktop\AOC\DAY_2\input";
+            string path = $@"..\..\..\..\testinput";
+            //string path = $@"..\..\..\..\input";
             ReadingInput(path);
 
             //int unsafecount = UnSafeNum();
             //int safecount = AOC.rows.Count - unsafecount;
 
-            int unsafecount = UnSafeNum_2();
-            int safecount = AOC.rows.Count - unsafecount;
+            int safecount = SafeNum(AOC.rows);
+            
             Console.WriteLine(safecount);
         }
     }
